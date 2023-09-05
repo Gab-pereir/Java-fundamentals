e92dcdac77c0ad64bfbe6cfa7b49facdee315e02
Gabriel Pereira de Alemida <pessoal.gabriel.pereira@gmail.com>
2023-09-05 11:40:12 -0300

    first commit

---
+++ Fundamentos/Primeiro programa/hello.java
@@ -0,0 +1,10 @@
+/**
+ Hello World
+ @author Gabriel Pereira de Almeida
+ */
+
+public class Hello{
+    public static void main(string[] args){
+        System.out.println("Hello,World!");
+    }
+}
